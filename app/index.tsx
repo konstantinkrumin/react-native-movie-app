a27438c1f35842a5f929ed3c4d1c2c44cfa78609
Konstantin Krumin <konstantin.krumin@gmail.com>
2025-03-19 13:14:08 -0400

    Trying routing examples

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import {Link} from "expo-router";
 
 export default function Index() {
   return (
@@ -6,6 +7,8 @@ export default function Index() {
       className="flex-1 justify-center items-center"
     >
       <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
+      <Link href='/onboarding'>Onboarding</Link>
+      <Link href='/movie/avengers'>Avenger Movie</Link>
     </View>
   );
 }
