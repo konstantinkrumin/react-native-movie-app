60e6f66c815803668c4f808e877d9da70e2da0e6
Konstantin Krumin <konstantin.krumin@gmail.com>
2025-03-19 00:27:16 -0400

    config color scheme

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
     <View
       className="flex-1 justify-center items-center"
     >
-      <Text className="text-5xl text-primary-500 font-bold">Welcome!</Text>
+      <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
     </View>
   );
 }
