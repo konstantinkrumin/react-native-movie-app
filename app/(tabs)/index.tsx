e418a34e739548369939dd8d5f7ae15f071c180b
Konstantin Krumin <konstantin.krumin@gmail.com>
2025-03-19 13:29:51 -0400

    Setting up routing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,4 @@
 import { Text, View } from "react-native";
-import {Link} from "expo-router";
 
 export default function Index() {
   return (
@@ -7,8 +6,6 @@ export default function Index() {
       className="flex-1 justify-center items-center"
     >
       <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
-      <Link href='/onboarding'>Onboarding</Link>
-      <Link href='/movie/avengers'>Avenger Movie</Link>
     </View>
   );
 }
