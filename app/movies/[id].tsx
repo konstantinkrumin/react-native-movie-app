03091f9bafebaa8e7421ef1c6518db10e56a9a2a
Konstantin Krumin <konstantin.krumin@gmail.com>
2025-03-21 14:30:30 -0400

    Hide status bar

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -22,7 +22,9 @@ interface MovieInfoProps {
 const MovieInfo = ({ label, value }: MovieInfoProps) => (
   <View className="flex-col items-start justify-center mt-5">
     <Text className="text-light-200 font-normal text-sm">{label}</Text>
-    <Text className="text-white font-bold text-sm mt-2">{value || "N/A"}</Text>
+    <Text className="text-light-100 font-bold text-sm mt-2">
+      {value || "N/A"}
+    </Text>
   </View>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,12 @@ module.exports = {
         primary: "#030014",
         secondary: "#151312",
         light: {
-          100: "#D6C6FF",
+          100: "#D6C7FF",
           200: "#A8B5DB",
           300: "#9CA4AB",
         },
         dark: {
-          100: "#221F4D",
+          100: "#221F3D",
           200: "#0F0D23",
         },
         accent: "#AB8BFF",
